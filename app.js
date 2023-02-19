ea555fdf6002452734e493d3393a0e203606df1a
jcasare <asarejerry16@gmail.com>
2023-02-19 20:00:10 +0000

    for loop with ejs

--- app.js
+++ app.js
@@ -8,19 +8,17 @@ const port = 3000 || process.env.PORT;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set("view engine", "ejs");
+let items = [];
 app.get("/", (req, res) => {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   const today = new Date();
-  const currentDay = today.getDay();
-  res.render("list", { kindOfDay: daysOfWeek[currentDay] });
+  const options = { weekday: "long", day: "numeric", month: "long" };
+  const currentDay = today.toLocaleString("en-US", options);
+  res.render("list", { kindOfDay: currentDay, newItems: items });
+});
+app.post("/api/v1/createActivity", async (req, res) => {
+  const newActivity = req.body.activity;
+  items.push(newActivity);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
